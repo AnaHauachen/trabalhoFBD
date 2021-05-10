b8e479bdfd2ea14c8d203e70bf904d5d9054ca98
Ana Lúcia de Morais Lima <ana.ufc@outlook.com>
2021-05-11 00:47:09 +0100

    Add files via upload

---
+++ Triggers.sql
@@ -0,0 +1,46 @@
+Use TBD_TRAB1
+
+-- --------------------------------------------------------
+
+--
+-- Criação Trigger RefreshUpdate
+--
+
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+ALTER TRIGGER [dbo].[InsertLog_FactLinha] ON [dbo].[FactLinha]
+		INSTEAD OF INSERT 
+		AS
+		BEGIN
+			SET NOCOUNT ON;
+
+			DECLARE
+				 @FactID varchar(30),
+				 @aux int
+		 
+				 SET @aux = (SELECT MAX(FacturaID)
+				 FROM [dbo].[FactLinha])
+
+				 SET @FactID = @aux + 1 + ':999'
+		
+
+			INSERT INTO [dbo].[LogOperations]
+						([EventType]
+						,[Objecto]
+						,[Valor]
+						,[Referencia])
+			VALUES
+				  ('I'
+				  ,'Begin'
+				  ,GETDATE()
+				  ,)
+			
+			
+			END
+			
+GO
+ALTER TABLE [dbo].[FactLinha] ENABLE TRIGGER [InsertLog_FactLinha]
+GO
